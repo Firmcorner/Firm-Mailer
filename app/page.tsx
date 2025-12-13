d6356d4087d6a0bdca1b72f42fe644cc4aff88a9
Hussain kagiwala <hussain.f.kagi@gmail.com>
2025-12-13 18:40:29 +0400

    loader added

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,7 @@ import { sendBulkEmails } from "./actions";
 import { EmailTemplates } from "@/components/email-templates";
 import { EmailPreview } from "@/components/email-preview";
 import { SEOContent } from "@/components/seo-content";
+import SplashLoader from "@/components/splashloader";
 
 // Type definitions
 interface FormData {
@@ -1008,6 +1009,8 @@ const FirmMailer: React.FC = () => {
           {/* SEO Content */}
           <div className="w-full">
             <SEOContent />
+
+            <SplashLoader isOpen={sending} />
           </div>
         </div>
       </div>
